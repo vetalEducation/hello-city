f995df889d9d6f3bca143435c95b814d8457b437
vetalEducation <tyu96@ukr.net>
2021-04-13 16:57:49 +0300

    Refactor hellokyiv.java.

--- hellokyiv.java
+++ hellokyiv.java
@@ -1,6 +1,6 @@
 public HelloKyiv {
 	public static void sayHello() {
-		System.in.println("Hello, Kyiv!!!");
+		System.out.println("Hello, Kyiv!!!");
 	}
 	
 	public static void main(String[] args) {
