41e61bf7f8f20477210e6cd6c0ba5c2f5ce5ec60
vetalEducation <tyu96@ukr.net>
2021-04-20 11:19:18 +0300

    Refactor hellolviv

--- hellolviv.java
+++ hellolviv.java
@@ -1,6 +1,6 @@
 public HelloLviv {
 	public static void sayHello() {
-		System.out.println("Привіт, Львів!!!");
+		System.out.println("Привіт, Львів!");
 	}
 	
 	public static void main(String[] args) {
