1a638a9f560c4e8151996c6ce5052a6a27a50c25
vetalEducation <tyu96@ukr.net>
2021-04-20 11:06:55 +0300

    Change language helloriasne2

--- helloriasne2.java
+++ helloriasne2.java
@@ -1,6 +1,6 @@
 public HelloRiasne2 {
 	public static void sayHello() {
-		System.out.println("Hello, Riasne-2!!!");
+		System.out.println("Привіт, Рясне-2!!!");
 	}
 	
 	public static void main(String[] args) {
