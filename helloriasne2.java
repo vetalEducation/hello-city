f247fd173617014988ab5afc4eebe10ba76e91aa
vetalEducation <tyu96@ukr.net>
2021-04-20 11:19:36 +0300

    Refactor helloriasne2

--- helloriasne2.java
+++ helloriasne2.java
@@ -1,6 +1,6 @@
 public HelloRiasne2 {
 	public static void sayHello() {
-		System.out.println("Привіт, Рясне-2!!!");
+		System.out.println("Привіт, Рясне-2!");
 	}
 	
 	public static void main(String[] args) {
